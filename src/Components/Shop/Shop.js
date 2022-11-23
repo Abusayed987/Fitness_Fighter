35a82d4d2013790edbc49ba36e18873002670c19
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-11-23 23:30:27 +0600

    first commit

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div className='shop-component'>
+            <div className='product-component'>
+
+            </div>
+            <div className='cart-component'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Shop;    
